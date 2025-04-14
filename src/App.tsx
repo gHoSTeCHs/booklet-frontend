4cbccd52ac9e1d4ef433fbecf53ba50dad9e8e3d
Somadina <kingswatter@gmail.com>
2025-04-14 15:39:11 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
-import Home from './pages/Home';
+import { Routes, Route, BrowserRouter as Router } from 'react-router';
+
+import ProtectedRoute from './components/ProtectedRoute';
+import { AuthProvider } from './context/AuthContext';
+import HomePage from './pages/Home';
+import HotelDetailPage from './pages/HotelDetailPage';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
+import SearchPage from './pages/SearchPage';
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="*" element={<Navigate to="/home" />} />
-				<Route path="/home" element={<Home />} />
-			</Routes>
-		</BrowserRouter>
+		<AuthProvider>
+			<Router>
+				<Routes>
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/register" element={<RegisterPage />} />
+					<Route
+						path="/"
+						element={
+							<ProtectedRoute>
+								<HomePage />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path="/search"
+						element={
+							<ProtectedRoute>
+								<SearchPage />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path="/hotel/:id"
+						element={
+							<ProtectedRoute>
+								<HotelDetailPage />
+							</ProtectedRoute>
+						}
+					/>
+				</Routes>
+			</Router>
+		</AuthProvider>
 	);
 }
 
